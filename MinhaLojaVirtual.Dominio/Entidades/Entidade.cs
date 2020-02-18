edba723219390280d70620b775389f7bff169fcd
Hermes Ribeiro <hermes.fatec@gmail.com>
2020-02-18 15:50:23 -0300

    Criação das Entidades

---
+++ MinhaLojaVirtual.Dominio/Entidades/Entidade.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinhaLojaVirtual.Dominio.Entidades
+{
+   public abstract class Entidade
+    {
+
+    }
+}
---
+++ MinhaLojaVirtual.Dominio/Entidades/Usuario.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace MinhaLojaVirtual.Dominio.Entidades
+{
+   public class Usuario
+    {
+        public int Id { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public string Nome { get; set; }
+        public string SobreNome { get; set; }
+
+        //um usuario pode ter muitos pedidos ou nenhum pedido
+        public ICollection<Pedido> Pedidos { get; set; }
+    }
+}
